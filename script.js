bac7cd4c6d5ba012ae464997908c01809f30febb
GiorgioDordo <djole6789@gmail.com>
2025-01-19 00:35:34 +0100

    Refactor button elements and add random color functionality

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ const green = document.getElementById("green");
 const red = document.getElementById("red");
 const blue = document.getElementById("blue");
 const random = document.getElementById("random");
+
 function setColor(name) {
     body.style.backgroundColor = name;
+}
+
+function randomColor() {
+    const red = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
+
+    const color = `rgb(${red},${green},${blue})`;
+
+    body.style.backgroundColor = color;
 }
 
 
@@ -22,7 +33,10 @@ blue.addEventListener("click", () => {
 })
 
 random.addEventListener("click", () => {
-    setColor("random");
+    randomColor();
 })
+
+// in alternativa agli addEventListener potevo utilizzare onclick direttamente sull'html,
+// ed asseggnare ad ogni elemento la propria funzione
 
 
