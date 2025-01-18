def0f3669e039fd2cc9739472f6fa7e325ba1484
GiorgioDordo <djole6789@gmail.com>
2025-01-18 22:45:12 +0100

    Add color change functionality to background based on button clicks

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 const body = document.getElementsByTagName("body")[0];
-body.style.backgroundColor = "red";
+const button = document.querySelectorAll("button");
+const green = document.getElementById("green");
+const red = document.getElementById("red");
+const blue = document.getElementById("blue");
+const random = document.getElementById("random");
+function setColor(name) {
+    body.style.backgroundColor = name;
+}
+
+
+green.addEventListener("click", () => {
+    setColor("green");
+})
+
+red.addEventListener("click", () => {
+    setColor("red");
+})
+
+blue.addEventListener("click", () => {
+    setColor("blue");
+})
+
+random.addEventListener("click", () => {
+    setColor("random");
+})
+
+
